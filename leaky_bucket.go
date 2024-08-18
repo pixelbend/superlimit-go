76ae35013958e7010514b6df20bc001ceac70a0a
driftdev <92285161+driftdev@users.noreply.github.com>
2024-08-18 19:38:00 +0530

    refactor: moved leaky bucket based limiter implementation to its own file

---
+++ leaky_bucket.go
@@ -0,0 +1,254 @@
+package surgelimit
+
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+	"strconv"
+)
+
+// LeakyBucketLimiter is the main struct that handles rate limiting. It uses a Redis client
+// to store and retrieve rate-limiting information and operates based on the
+// provided Options.
+//
+// Fields:
+//   - client: A Redis client used to interact with the Redis or Redis Compatible database.
+//   - Options: Configuration options for the LeakyBucketLimiter
+type LeakyBucketLimiter struct {
+	client  redis.UniversalClient
+	Options Options
+}
+
+// NewLeakyBucketLimiter creates and returns a new LeakyBucketLimiter instance using the provided
+// Redis client and options. The LeakyBucketLimiter is responsible for managing rate limits
+// based on the given configuration.
+//
+// Parameters:
+//   - client: A Redis UniversalClient used to interact with the Redis database.
+//   - options: An Options struct that configures the LeakyBucketLimiter, including the key prefix.
+//
+// Returns:
+//   - *LeakyBucketLimiter: A pointer to the newly created LeakyBucketLimiter instance.
+//
+// Example:
+//
+//	redisClient := redis.NewUniversalClient(&redis.UniversalOptions{
+//	    // Redis client configuration
+//	})
+//
+//	// Create a limiter with default options
+//	limiter := NewLimiter(redisClient, DefaultOptions())
+//
+//	// Or create a limiter with custom options
+//	customOptions := Options{KeyPrefix: "CUSTOM_PREFIX"}
+//	limiter := NewLimiter(redisClient, customOptions)
+func NewLeakyBucketLimiter(client redis.UniversalClient, options Options) *LeakyBucketLimiter {
+	return &LeakyBucketLimiter{
+		client:  client,
+		Options: options,
+	}
+}
+
+// Allow attempts to allow a single request for a given key under a rate-limiting
+// scheme defined by the `surgelimit.Limit` struct. This is a convenience method that
+// calls `AllowN` with `n` set to 1, meaning it checks if just one request can
+// be made at the current time.
+//
+// Parameters:
+//   - ctx: The context to control cancellation and timeouts.
+//   - key: The unique identifier for the entity being rate-limited (e.g., user ID, IP address).
+//   - limit: A `surgelimit.Limit` struct defining the rate (requests per period) and burst (maximum burst size).
+//     This can be created using helper functions such as `surgelimit.PerSecond`, `surgelimit.PerMinute`, or `surgelimit.PerHour`.
+//
+// Returns:
+//   - *Result: A struct containing information about the rate limiting state, such as the number
+//     of requests allowed, remaining, and times for retry and reset.
+//   - error: If an error occurs while executing the underlying `AllowN` method or parsing the result, it is returned.
+//
+// Example:
+//
+//	// Create a rate limit of 5 requests per second
+//	limit := surgelimit.PerSecond(5)
+//
+//	result, err := Allow(ctx, "user_1234", limit)
+//	if err != nil {
+//	    log.Fatalf("Failed to check rate limit: %v", err)
+//	}
+//
+//	if result.Allowed > 0 {
+//	    log.Println("Request allowed.")
+//	} else {
+//	    log.Printf("Rate limit exceeded. Retry after %v, reset after %v.", result.RetryAfter, result.ResetAfter)
+//	}
+func (l *LeakyBucketLimiter) Allow(ctx context.Context, key string, limit Limit) (*Result, error) {
+	return l.AllowN(ctx, key, limit, 1)
+}
+
+// AllowN attempts to allow `n` requests for a given key under a rate-limiting
+// scheme defined by the `Limit` struct. This method uses a Lua script to
+// calculate whether the requests can be allowed based on the current rate limit state.
+//
+// The `surgelimit.Limit` struct can be easily created using helper functions like `surgelimit.PerSecond`,
+// `surgelimit.PerMinute`, and `surgelimit.PerHour`, which define the rate limit in terms of requests
+// per second, minute, or hour respectively. Each helper function sets the `Burst`
+// to be equal to the `Rate`, meaning that the system can handle a burst of up to
+// `Rate` requests in one period.
+//
+// Parameters:
+//   - ctx: The context to control cancellation and timeouts.
+//   - key: The unique identifier for the entity being rate-limited (e.g., user ID, IP address).
+//   - limit: A `surgelimit.Limit` struct defining the rate (requests per period) and burst (maximum burst size).
+//   - n: The number of requests to attempt to allow.
+//
+// Returns:
+//   - *Result: A struct containing information about the rate limiting state, such as the number
+//     of requests allowed, remaining, and times for retry and reset.
+//   - error: If an error occurs while executing the Lua script or parsing the result, it is returned.
+//
+// Example:
+//
+//	// Create a rate limit of 5 requests per second
+//	limit := surgelimit.PerSecond(10)
+//
+//	result, err := AllowN(ctx, "user_1234", limit, 3)
+//	if err != nil {
+//	    log.Fatalf("Failed to check rate limit: %v", err)
+//	}
+//
+//	if result.Allowed > 0 {
+//	    log.Printf("Allowed %d requests, %d remaining.", result.Allowed, result.Remaining)
+//	} else {
+//	    log.Printf("Rate limit exceeded. Retry after %v, reset after %v.", result.RetryAfter, result.ResetAfter)
+//	}
+func (l *LeakyBucketLimiter) AllowN(
+	ctx context.Context,
+	key string,
+	limit Limit,
+	n int,
+) (*Result, error) {
+	values := []interface{}{limit.Burst, limit.Rate, limit.Period.Seconds(), n}
+	v, err := allowN.Run(ctx, l.client, []string{keyWithPrefix(l.Options.KeyPrefix, key)}, values...).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	values = v.([]interface{})
+
+	retryAfter, err := strconv.ParseFloat(values[2].(string), 64)
+	if err != nil {
+		return nil, err
+	}
+
+	resetAfter, err := strconv.ParseFloat(values[3].(string), 64)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &Result{
+		Limit:      limit,
+		Allowed:    int(values[0].(int64)),
+		Remaining:  int(values[1].(int64)),
+		RetryAfter: dur(retryAfter),
+		ResetAfter: dur(resetAfter),
+	}
+	return res, nil
+}
+
+// AllowAtMost attempts to allow up to `n` requests for a given key under a rate-limiting
+// scheme defined by the `surgelimit.Limit` struct. This method is similar to `AllowN`, but with
+// the focus on allowing the maximum possible number of requests up to the limit `n`
+// without exceeding the rate limit.
+//
+// The `surgelimit.Limit` struct can be easily created using helper functions like `surgelimit.PerSecond`,
+// `surgelimit.PerMinute`, and `surgelimit.PerHour`, which define the rate limit in terms of requests
+// per second, minute, or hour respectively. Each helper function sets the `Burst`
+// to be equal to the `Rate`, meaning that the system can handle a burst of up to
+// `Rate` requests in one period.
+//
+// Parameters:
+//   - ctx: The context to control cancellation and timeouts.
+//   - key: The unique identifier for the entity being rate-limited (e.g., user ID, IP address).
+//   - limit: A `surgelimit.Limit` struct defining the rate (requests per period) and burst (maximum burst size).
+//     This can be created using helper functions such as `surgelimit.PerSecond`, `surgelimit.PerMinute`, or `surgelimit.PerHour`.
+//   - n: The maximum number of requests to attempt to allow.
+//
+// Returns:
+//   - *Result: A struct containing information about the rate limiting state, such as the number
+//     of requests actually allowed, remaining, and times for retry and reset.
+//   - error: If an error occurs while executing the Lua script or parsing the result, it is returned.
+//
+// Example:
+//
+//	// Create a rate limit of 10 requests per minute
+//	limit := surgelimit.PerMinute(10)
+//
+//	// Attempt to allow up to 5 requests
+//	result, err := AllowAtMost(ctx, "user_1234", limit, 5)
+//	if err != nil {
+//	    log.Fatalf("Failed to check rate limit: %v", err)
+//	}
+//
+//	log.Printf("Allowed %d requests, %d remaining.", result.Allowed, result.Remaining)
+//	if result.Allowed < 5 {
+//	    log.Printf("Rate limit exceeded for some requests. Retry after %v, reset after %v.", result.RetryAfter, result.ResetAfter)
+//	}
+func (l *LeakyBucketLimiter) AllowAtMost(
+	ctx context.Context,
+	key string,
+	limit Limit,
+	n int,
+) (*Result, error) {
+	values := []interface{}{limit.Burst, limit.Rate, limit.Period.Seconds(), n}
+	v, err := allowAtMost.Run(ctx, l.client, []string{keyWithPrefix(l.Options.KeyPrefix, key)}, values...).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	values = v.([]interface{})
+
+	retryAfter, err := strconv.ParseFloat(values[2].(string), 64)
+	if err != nil {
+		return nil, err
+	}
+
+	resetAfter, err := strconv.ParseFloat(values[3].(string), 64)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &Result{
+		Limit:      limit,
+		Allowed:    int(values[0].(int64)),
+		Remaining:  int(values[1].(int64)),
+		RetryAfter: dur(retryAfter),
+		ResetAfter: dur(resetAfter),
+	}
+	return res, nil
+}
+
+// Reset clears the rate limiter's state for a given key, effectively resetting
+// any rate limiting associated with that key. This can be useful if you want to
+// manually clear the rate limits for a specific user or action, for example,
+// after a penalty period has passed or after a successful manual intervention.
+//
+// The method removes the key from the underlying storage, which
+// effectively resets the rate limiting data (e.g., the number of requests made
+// and the timestamps) for that key.
+//
+// Parameters:
+//   - ctx: The context to control cancellation and timeouts.
+//   - key: The unique identifier for the entity whose rate limit is to be reset.
+//
+// Returns:
+//   - error: If an error occurs during the deletion process, it is returned.
+//     If the operation is successful, it returns nil.
+//
+// Example:
+//
+//	// Reset the rate limit for user ID 1234
+//	err := Reset(ctx, "user_1234")
+//	if err != nil {
+//		log.Printf("Warning: Failed to reset rate limiter for user_1234: %v", err)
+//	}
+func (l *LeakyBucketLimiter) Reset(ctx context.Context, key string) error {
+	return l.client.Del(ctx, keyWithPrefix(l.Options.KeyPrefix, key)).Err()
+}
