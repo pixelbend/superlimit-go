6d92dc7faec4ce0ae9a5b72763bdcd1a65d17d01
driftdev <92285161+driftdev@users.noreply.github.com>
2024-08-19 20:22:45 +0530

    refactor: moved leaky bucket implementation to its own module

--- leakybucket/leakybucket.go
+++ leakybucket/leakybucket.go
@@ -1,4 +1,4 @@
-package surgelimit
+package leakybucket
 
 import (
 	"context"
@@ -6,186 +6,50 @@ import (
 	"strconv"
 )
 
-// allowN is a Redis Lua script used for handling rate limiting based on the number of requests.
-// It calculates how many of the requested `n` requests can be allowed given the current rate limit state.
-//
-// The script performs the following operations:
-//   - Retrieves the current time and calculates time-based values for rate limiting.
-//   - Checks the existing rate limit state stored in Redis.
-//   - Determines the number of requests allowed based on the provided cost and rate limit parameters.
-//   - Updates the rate limit state in Redis and returns information on allowed requests, remaining requests,
-//     and the times to retry or reset the rate limit.
-//
-// Parameters used in the script:
-//   - KEYS[1]: The key used for rate limiting in Redis.
-//   - ARGV[1]: The burst size (maximum number of requests that can be handled in a burst).
-//   - ARGV[2]: The rate (number of requests allowed per period).
-//   - ARGV[3]: The period (duration in seconds for the rate limit).
-//   - ARGV[4]: The cost (number of requests being attempted).
-//
-// Example usage in Go:
-//
-//	result, err := allowN.Run(ctx, redisClient, []string{"RATE_LIMIT:user_1234"}, burst, rate, period.Seconds(), n).Result()
-//	// Handle result and error
-var allowN = redis.NewScript(`
-redis.replicate_commands()
+const keyPrefix = "LEAKY_LIMIT"
 
-local rate_limit_key = KEYS[1]
-local burst = ARGV[1]
-local rate = ARGV[2]
-local period = ARGV[3]
-local cost = tonumber(ARGV[4])
-
-local emission_interval = period / rate
-local increment = emission_interval * cost
-local burst_offset = emission_interval * burst
-
-local jan_1_2017 = 1483228800
-local now = redis.call("TIME")
-now = (now[1] - jan_1_2017) + (now[2] / 1000000)
-
-local tat = redis.call("GET", rate_limit_key)
-
-if not tat then
-  tat = now
-else
-  tat = tonumber(tat)
-end
-
-tat = math.max(tat, now)
-
-local new_tat = tat + increment
-local allow_at = new_tat - burst_offset
-
-local diff = now - allow_at
-local remaining = diff / emission_interval
-
-if remaining < 0 then
-  local reset_after = tat - now
-  local retry_after = diff * -1
-  return {
-    0, -- allowed
-    0, -- remaining
-    tostring(retry_after),
-    tostring(reset_after),
-  }
-end
-
-local reset_after = new_tat - now
-if reset_after > 0 then
-  redis.call("SET", rate_limit_key, new_tat, "EX", math.ceil(reset_after))
-end
-local retry_after = -1
-return {cost, remaining, tostring(retry_after), tostring(reset_after)}
-`)
-
-// allowAtMost is a Redis Lua script used for handling rate limiting based on the maximum number of requests.
-// It calculates the maximum number of requests that can be allowed, considering the burst capacity
-// and rate limit state. The script ensures that no more than the allowed number of requests is processed.
-//
-// The script performs the following operations:
-//   - Retrieves the current time and calculates time-based values for rate limiting.
-//   - Checks the existing rate limit state stored in Redis.
-//   - Determines the maximum number of requests that can be allowed based on the provided cost and rate limit parameters.
-//   - Updates the rate limit state in Redis and returns information on allowed requests, remaining requests,
-//     and the times to retry or reset the rate limit.
-//
-// Parameters used in the script:
-//   - KEYS[1]: The key used for rate limiting in Redis.
-//   - ARGV[1]: The burst size (maximum number of requests that can be handled in a burst).
-//   - ARGV[2]: The rate (number of requests allowed per period).
-//   - ARGV[3]: The period (duration in seconds for the rate limit).
-//   - ARGV[4]: The cost (number of requests being attempted).
-//
-// Example usage in Go:
-//
-//	result, err := allowAtMost.Run(ctx, redisClient, []string{"RATE_LIMIT:user_1234"}, burst, rate, period.Seconds(), n).Result()
-//	// Handle result and error
-var allowAtMost = redis.NewScript(`
-redis.replicate_commands()
-
-local rate_limit_key = KEYS[1]
-local burst = ARGV[1]
-local rate = ARGV[2]
-local period = ARGV[3]
-local cost = tonumber(ARGV[4])
-
-local emission_interval = period / rate
-local burst_offset = emission_interval * burst
-
-local jan_1_2017 = 1483228800
-local now = redis.call("TIME")
-now = (now[1] - jan_1_2017) + (now[2] / 1000000)
-
-local tat = redis.call("GET", rate_limit_key)
-
-if not tat then
-  tat = now
-else
-  tat = tonumber(tat)
-end
-
-tat = math.max(tat, now)
-
-local diff = now - (tat - burst_offset)
-local remaining = diff / emission_interval
-
-if remaining < 1 then
-  local reset_after = tat - now
-  local retry_after = emission_interval - diff
-  return {
-    0, -- allowed
-    0, -- remaining
-    tostring(retry_after),
-    tostring(reset_after),
-  }
-end
-
-if remaining < cost then
-  cost = remaining
-  remaining = 0
-else
-  remaining = remaining - cost
-end
-
-local increment = emission_interval * cost
-local new_tat = tat + increment
-
-local reset_after = new_tat - now
-if reset_after > 0 then
-  redis.call("SET", rate_limit_key, new_tat, "EX", math.ceil(reset_after))
-end
-
-return {
-  cost,
-  remaining,
-  tostring(-1),
-  tostring(reset_after),
+// Options holds the configuration settings for the Limiter.
+type Options struct {
+	// KeyPrefix is the prefix added to all keys used by the limiter.
+	// This helps to distinguish rate-limiting keys from other keys.
+	// By default, it is set to "LEAKY_LIMIT", but it can be customized to avoid key collisions
+	KeyPrefix string
 }
-`)
 
-// LeakyBucketLimiter is the main struct that handles rate limiting. It uses a Redis client
+// DefaultOptions returns an Options struct populated with the default settings
+// for the Limiter. Currently, this includes setting the `KeyPrefix` to "LEAKY_LIMIT".
+//
+// Defaults:
+//
+//   - KeyPrefix: The prefix used for keys managed by the limiter. Default is set to "LEAKY_LIMIT".
+func DefaultOptions() Options {
+	return Options{
+		KeyPrefix: keyPrefix,
+	}
+}
+
+// Limiter is the main struct that handles rate limiting. It uses a Redis client
 // to store and retrieve rate-limiting information and operates based on the
 // provided Options.
 //
 // Fields:
 //   - client: A Redis client used to interact with the Redis or Redis Compatible database.
-//   - Options: Configuration options for the LeakyBucketLimiter
-type LeakyBucketLimiter struct {
+//   - Options: Configuration options for the Limiter
+type Limiter struct {
 	client  redis.UniversalClient
 	Options Options
 }
 
-// NewLeakyBucketLimiter creates and returns a new LeakyBucketLimiter instance using the provided
-// Redis client and options. The LeakyBucketLimiter is responsible for managing rate limits
+// NewLimiter creates and returns a new Limiter instance using the provided
+// Redis client and options. The Limiter is responsible for managing rate limits
 // based on the given configuration.
 //
 // Parameters:
 //   - client: A Redis UniversalClient used to interact with the Redis database.
-//   - options: An Options struct that configures the LeakyBucketLimiter, including the key prefix.
+//   - options: An Options struct that configures the Limiter, including the key prefix.
 //
 // Returns:
-//   - *LeakyBucketLimiter: A pointer to the newly created LeakyBucketLimiter instance.
+//   - *Limiter: A pointer to the newly created Limiter instance.
 //
 // Example:
 //
@@ -199,8 +63,8 @@ type LeakyBucketLimiter struct {
 //	// Or create a limiter with custom options
 //	customOptions := Options{KeyPrefix: "CUSTOM_PREFIX"}
 //	limiter := NewLimiter(redisClient, customOptions)
-func NewLeakyBucketLimiter(client redis.UniversalClient, options Options) *LeakyBucketLimiter {
-	return &LeakyBucketLimiter{
+func NewLimiter(client redis.UniversalClient, options Options) *Limiter {
+	return &Limiter{
 		client:  client,
 		Options: options,
 	}
@@ -215,7 +79,7 @@ func NewLeakyBucketLimiter(client redis.UniversalClient, options Options) *Leaky
 //   - ctx: The context to control cancellation and timeouts.
 //   - key: The unique identifier for the entity being rate-limited (e.g., user ID, IP address).
 //   - limit: A `surgelimit.Limit` struct defining the rate (requests per period) and burst (maximum burst size).
-//     This can be created using helper functions such as `surgelimit.PerSecond`, `surgelimit.PerMinute`, or `surgelimit.PerHour`.
+//     This can be created using helper functions such as `surgelimit.LimitPerSecond`, `surgelimit.LimitPerMinute`, or `surgelimit.LimitPerHour`.
 //
 // Returns:
 //   - *Result: A struct containing information about the rate limiting state, such as the number
@@ -225,7 +89,7 @@ func NewLeakyBucketLimiter(client redis.UniversalClient, options Options) *Leaky
 // Example:
 //
 //	// Create a rate limit of 5 requests per second
-//	limit := surgelimit.PerSecond(5)
+//	limit := surgelimit.LimitPerSecond(5)
 //
 //	result, err := Allow(ctx, "user_1234", limit)
 //	if err != nil {
@@ -237,7 +101,7 @@ func NewLeakyBucketLimiter(client redis.UniversalClient, options Options) *Leaky
 //	} else {
 //	    log.Printf("Rate limit exceeded. Retry after %v, reset after %v.", result.RetryAfter, result.ResetAfter)
 //	}
-func (l *LeakyBucketLimiter) Allow(ctx context.Context, key string, limit Limit) (*Result, error) {
+func (l *Limiter) Allow(ctx context.Context, key string, limit Limit) (*Result, error) {
 	return l.AllowN(ctx, key, limit, 1)
 }
 
@@ -245,8 +109,8 @@ func (l *LeakyBucketLimiter) Allow(ctx context.Context, key string, limit Limit)
 // scheme defined by the `Limit` struct. This method uses a Lua script to
 // calculate whether the requests can be allowed based on the current rate limit state.
 //
-// The `surgelimit.Limit` struct can be easily created using helper functions like `surgelimit.PerSecond`,
-// `surgelimit.PerMinute`, and `surgelimit.PerHour`, which define the rate limit in terms of requests
+// The `surgelimit.Limit` struct can be easily created using helper functions like `surgelimit.LimitPerSecond`,
+// `surgelimit.LimitPerMinute`, and `surgelimit.LimitPerHour`, which define the rate limit in terms of requests
 // per second, minute, or hour respectively. Each helper function sets the `Burst`
 // to be equal to the `Rate`, meaning that the system can handle a burst of up to
 // `Rate` requests in one period.
@@ -265,7 +129,7 @@ func (l *LeakyBucketLimiter) Allow(ctx context.Context, key string, limit Limit)
 // Example:
 //
 //	// Create a rate limit of 5 requests per second
-//	limit := surgelimit.PerSecond(10)
+//	limit := surgelimit.LimitPerSecond(10)
 //
 //	result, err := AllowN(ctx, "user_1234", limit, 3)
 //	if err != nil {
@@ -277,7 +141,7 @@ func (l *LeakyBucketLimiter) Allow(ctx context.Context, key string, limit Limit)
 //	} else {
 //	    log.Printf("Rate limit exceeded. Retry after %v, reset after %v.", result.RetryAfter, result.ResetAfter)
 //	}
-func (l *LeakyBucketLimiter) AllowN(
+func (l *Limiter) AllowN(
 	ctx context.Context,
 	key string,
 	limit Limit,
@@ -316,8 +180,8 @@ func (l *LeakyBucketLimiter) AllowN(
 // the focus on allowing the maximum possible number of requests up to the limit `n`
 // without exceeding the rate limit.
 //
-// The `surgelimit.Limit` struct can be easily created using helper functions like `surgelimit.PerSecond`,
-// `surgelimit.PerMinute`, and `surgelimit.PerHour`, which define the rate limit in terms of requests
+// The `surgelimit.Limit` struct can be easily created using helper functions like `surgelimit.LimitPerSecond`,
+// `surgelimit.LimitPerMinute`, and `surgelimit.LimitPerHour`, which define the rate limit in terms of requests
 // per second, minute, or hour respectively. Each helper function sets the `Burst`
 // to be equal to the `Rate`, meaning that the system can handle a burst of up to
 // `Rate` requests in one period.
@@ -326,7 +190,7 @@ func (l *LeakyBucketLimiter) AllowN(
 //   - ctx: The context to control cancellation and timeouts.
 //   - key: The unique identifier for the entity being rate-limited (e.g., user ID, IP address).
 //   - limit: A `surgelimit.Limit` struct defining the rate (requests per period) and burst (maximum burst size).
-//     This can be created using helper functions such as `surgelimit.PerSecond`, `surgelimit.PerMinute`, or `surgelimit.PerHour`.
+//     This can be created using helper functions such as `surgelimit.LimitPerSecond`, `surgelimit.LimitPerMinute`, or `surgelimit.LimitPerHour`.
 //   - n: The maximum number of requests to attempt to allow.
 //
 // Returns:
@@ -337,7 +201,7 @@ func (l *LeakyBucketLimiter) AllowN(
 // Example:
 //
 //	// Create a rate limit of 10 requests per minute
-//	limit := surgelimit.PerMinute(10)
+//	limit := surgelimit.LimitPerMinute(10)
 //
 //	// Attempt to allow up to 5 requests
 //	result, err := AllowAtMost(ctx, "user_1234", limit, 5)
@@ -349,7 +213,7 @@ func (l *LeakyBucketLimiter) AllowN(
 //	if result.Allowed < 5 {
 //	    log.Printf("Rate limit exceeded for some requests. Retry after %v, reset after %v.", result.RetryAfter, result.ResetAfter)
 //	}
-func (l *LeakyBucketLimiter) AllowAtMost(
+func (l *Limiter) AllowAtMost(
 	ctx context.Context,
 	key string,
 	limit Limit,
@@ -407,6 +271,6 @@ func (l *LeakyBucketLimiter) AllowAtMost(
 //	if err != nil {
 //		log.Printf("Warning: Failed to reset rate limiter for user_1234: %v", err)
 //	}
-func (l *LeakyBucketLimiter) Reset(ctx context.Context, key string) error {
+func (l *Limiter) Reset(ctx context.Context, key string) error {
 	return l.client.Del(ctx, keyWithPrefix(l.Options.KeyPrefix, key)).Err()
 }
